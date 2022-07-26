821bd9da488dcea8e1fc8543a8f19bccdba472bf
Mike Eichler <86664474+mikeeichler@users.noreply.github.com>
2022-07-26 20:33:46 +0100

    Update Controller.java

--- src/main/java/bianova/bianova/Controller.java
+++ src/main/java/bianova/bianova/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+//@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class Controller {
 
