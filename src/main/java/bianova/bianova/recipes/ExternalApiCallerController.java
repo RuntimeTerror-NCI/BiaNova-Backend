f4199df3cf92eb1f8abef566a8cbacbebeb3f1b6
Mike Eichler <86664474+mikeeichler@users.noreply.github.com>
2022-07-26 18:18:59 +0100

    Update ExternalApiCallerController.java

--- src/main/java/bianova/bianova/recipes/ExternalApiCallerController.java
+++ src/main/java/bianova/bianova/recipes/ExternalApiCallerController.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+@CrossOrigin
 @RestController
 public class ExternalApiCallerController {
 
